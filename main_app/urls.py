776c923609b5cc1bd840cb6cb5416a568d7e6a9e
pmorariu <morariupetru@yahoo.com>
2025-03-27 20:51:30 +0200

    Cursul din 27.03 show delete, view books for a logged in user

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,13 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from . import views
-from .views import BookViewSet, BookCustomViewSet, ActionViewSet, PizzaCustomViewSet
+from .views import BookViewSet, BookCustomViewSet, ActionViewSet
 
 router = DefaultRouter()
-router.register('books', BookViewSet, basename='book')
+router.register('books_api', BookViewSet, basename='book')
 router.register('custom_books', BookCustomViewSet, basename="custom_book")
 router.register('common_elements', ActionViewSet, basename="common_elements")
-router.register('pizza', PizzaCustomViewSet, basename='pizza')
 
 urlpatterns = [
     # path("", views.home, name="home"),
@@ -17,6 +16,12 @@ urlpatterns = [
     # path("api/books", views.get_books, name="get_books"),
     # path("books", views.get_books_page, name="books_page")
     path("", include(router.urls)),
-    path("home/", views.home, name="home")
+    path("home/", views.home, name="home"),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/', views.my_books, name='my_books'),
+    path('books/delete/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('books/confirm_delete/<int:book_id>/', views.confirm_delete, name='confirm_delete')
 ]
+
+
 
