5053102b2681104aaf5ceb409f38a6d4a1d56092
pmorariu <morariupetru@yahoo.com>
2025-04-01 09:45:29 +0300

    add profile for the users and config /admin page to manages users, profiles and other models you have created

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.registration_view, name='register'),
+    path('profile/', views.profile, name='profile'),
     path('login/', views.login_view, name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import LOGIN_REDIRECT_URL, LOGOUT_REDIRECT_URL
@@ -130,4 +130,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
